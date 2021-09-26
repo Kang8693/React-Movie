c3f34e9dce42c7478a55029477e9424c3739c1d8
Kang8693 <kbc980223@naver.com>
2021-09-26 23:36:24 +0900

    공부중...

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,16 +1,15 @@
 /* eslint-disable */
 
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   state = {
     isLoading: true
-  };
+  };a
 
   componentDidMount() {
-    setTimeout( () => {
-      this.setState({isLoading: false});
-    }, 5000);
+    axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
   }
 
   render() {
